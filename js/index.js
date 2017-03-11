b693f11df86a293e4bf3984974e7bba8c403a1a6
xupanqiang <541592004@qq.com>
2017-03-11 21:22:36 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,46 +1,30 @@
-// $(function() {
-//     //导航
-//     $(".deep-h-nav li").ready(function() {
-//         var index = 0;
-//         var adHeight = $(".deep-main").height();
-//         $(".deep-h-nav li").click(function() {
-//             index = $(".deep-h-nav li").index(this);
-//             $("body,html").stop(true, false).animate({ scrollTop: adHeight * index }, 500); //stop可选。规定是否停止被选元素的所有加入队列的动画。可选。规定是否允许完成当前的动画。
-//         });
-//         $(window).scroll(function() {
-//             var top = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop,
-//                 len = $(".deep-h-nav li").length;
-//             for (i = 0; i < len; i++) {
-//                 if (top >= (adHeight * i)-200) {
-//                     $(".deep-h-nav li").removeClass("current").eq(i).addClass("current");
-//                 }
-//                 if(top>=(adHeight-200)){
-//                     $(".deep-header").addClass("ba-hadeer");
-//                 } else {
-//                     $(".deep-header").removeClass("ba-hadeer");
-//                 }
-//                 if(top>=(adHeight*2)&&top<(adHeight*3)){
-//                     $(".about-cen").addClass("about-c01");
-//                     $(".about-cl").addClass("about-c02");
-//                     $(".about-ln").addClass("about-c02");
-//                     $(".about-zz").addClass("about-c03");
-//                     $(".about-fx").addClass("about-c03");
-//                 } else{
-//                     $(".about-cen").removeClass("about-c01");
-//                     $(".about-cl").removeClass("about-c02");
-//                     $(".about-ln").removeClass("about-c02");
-//                     $(".about-zz").removeClass("about-c03");
-//                     $(".about-fx").removeClass("about-c03");
-//                 }
-//             }
-//         });
-// 		//点击滚动到第二屏
-//         $(".main-xia img").click(function(){
-//             $("body,html").stop(true, false).animate({ scrollTop: adHeight }, 500);
-//         })
-//     });
-//     //回到顶部
-//     $(".gototop").click(function() {
-//         $("body,html").stop(true, false).animate({ scrollTop: 0 }, 1000);
-//     });
-// })
+$(function(){
+	// $.ajax({
+	// 	type: "GET",
+	// 	url: "ajax/index.json",
+	// 	dataType: "json",
+	// 	success: function(data){
+	// 		for(var i in data.about_info){
+	// 			$("#about_info").append("<p>"+data.about_info[i]+"</p>");
+	// 		}
+	// 		$("#skill_info").append("<p>"+data.skill_info+"</p>");
+	// 		$(".skill_int").append("<ul></ul>");
+	// 	},
+	// });
+	//获取浏览器宽度判断是否小于1024
+    var _width = $(window).width();
+    if (_width < 1024) {
+        alert("暂时小于1024屏幕暂时没有实现");
+    } else {
+     	// 我的经历
+  		$(".num li").click(function() {
+            var index = $(".num li").index(this);
+            $(this).addClass("on").siblings().removeClass("on")
+            showImg(index);
+        });
+        function showImg(index){
+		    var adHeight = $(".slider li").height();
+		    $(".slider").stop(true,false).animate({"margin-top" : -adHeight*index},500);//stop可选。规定是否停止被选元素的所有加入队列的动画。可选。规定是否允许完成当前的动画
+		}
+    }
+});
