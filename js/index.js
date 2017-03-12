c1d5bfbaa54fb06451f1b169e32eb395b666de0b
xupanqiang <541592004@qq.com>
2017-03-12 20:12:59 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -60,12 +60,50 @@ $(function(){
 	
 	
 	
+
+	
+	
+	
+	
+	
+	
 	
 	//获取浏览器宽度判断是否小于1024
     var _width = $(window).width();
     if (_width < 1024) {
         alert("暂时小于1024屏幕暂时没有实现");
     } else {
+    	
+    	
+	    //公告部分 ---离线储存
+	    //点击小差号，关闭弹出层
+	    $('.closenotice').on('click',function(){
+	        storeDisplay();  //存储信息，将存储属性设置为"1"
+	        $('.upgradeNotice').hide();
+	    });
+	    //本地存储访客是否第一次访问
+	    var strModel="value";
+	    //公告调用
+	    var storeDisplay=function(){
+	    var modelDisplay=1;      //定义存储对象的属性值
+	    //存储，IE6~IE7 cookie 其他浏览器HTML5本地存储
+	    if(window.localStorage) {
+	        localStorage.setItem(strModel,modelDisplay);  //进行本地存储
+	        } else {
+	        Cookie.write(strModel,modelDisplay);   //进行Cookie存储
+	        }
+	    };
+	    //检测触发是否显示弹窗
+	    var strStoreDate=window.localStorage ? localStorage.getItem(strModel) : Cookie.read(strModel);      //检测浏览器是否支持localStorage
+	    if(strStoreDate != "1"){    //如果属性值不为1则显示弹框
+	        $('.upgradeNotice').show();
+	    } else {
+	    	$('.upgradeNotice').hide();
+	    }
+    	
+    	
+    	
+    	
      	// 我的经历轮播
   		$(".num li").click(function() {
             var index = $(".num li").index(this);
@@ -76,5 +114,46 @@ $(function(){
 		    var adHeight = $(".slider li").height();
 		    $(".slider").stop(true,false).animate({"margin-top" : -adHeight*index},500);//stop可选。规定是否停止被选元素的所有加入队列的动画。可选。规定是否允许完成当前的动画
 		}
+        
+        //首页出现
+        $(".header").animate({opacity:"1"},1000,function(){
+        	$(".loimg img").animate({opacity:"1"},1000,function(){
+				$(".loimg h2").animate({opacity:"1"},1000,function(){
+					$(".loimg p").addClass("sh");
+				});
+			});
+		});
+		
+		
+		
+		//整屏幕滚动
+		var runPage;
+		runPage = new FullPage({
+		    id: 'pageContain',
+		    slideTime: 800,
+		    effect: {
+		        transform: {
+		            translate: 'Y'
+		        },
+		        opacity: [0, 1]
+		    },
+		    mode: 'wheel, touch, nav:navBar',
+		    easing: 'ease',
+		    callback: function(index, thisPage){
+		        if(index==0){
+		           // alert("第一屏");
+		        }
+		        if(index==1){
+		            // alert("第二屏");
+		        }
+		    }
+		});
+		
+		
+        
     }
+    
+
+
+
 });
