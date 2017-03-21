239aa028f697ec175cf49f0c7573de3e62f28740
xupanqiang <541592004@qq.com>
2017-03-21 16:45:45 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -153,11 +153,11 @@ $(function(){
     
 	window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {   
         if (window.orientation === 180 || window.orientation === 0) {    
-            //alert('竖屏状态！');
+            alert('竖屏状态！');
             window.addEventListener("onorientationchange"  in window  ? "orientationchange" : "resize",hengpin,false);
         }    
         if (window.orientation === 90 || window.orientation === -90 ){    
-            //alert('横屏状态！');   
+            alert('横屏状态！');   
         }     
     }, false);   
 
